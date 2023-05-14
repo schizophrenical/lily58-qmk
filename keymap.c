2fc8dcb3277185855b3bcda8a6cce99c1cd8e635
schizophrenical <schizophrenical@pm.me>
2023-05-14 22:11:43 +0800

    QWERTY Keymap - Added tilde and del keys.

--- keymap.c
+++ keymap.c
@@ -13,9 +13,6 @@ Features:
 
 #include QMK_KEYBOARD_H
 
-//Ctl Alt Del
-#define KC_CAD	LALT(LCTL(KC_DEL))
-
 #ifdef BONGO_ENABLE
 #include "bongo.h"
 #endif
@@ -33,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  QWERTY
  *
  *  ╭────────┬────────┬────────┬────────┬────────┬────────╮                         ╭────────┬────────┬────────┬────────┬────────┬────────╮
- *  ├    `   ┼   1    ┼   2    ┼   3    ┼   4    ┼   5    ┤                         ├   6    ┼   7    ┼   8    ┼   9    ┼   0    ┼  ENC   ┤
+ *  ├    `   ┼   1    ┼   2    ┼   3    ┼   4    ┼   5    ┤                         ├   6    ┼   7    ┼   8    ┼   9    ┼   0    ┼    ~   ┤
  *  ├────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┤
- *  ├   TAB  ┼   Q    ┼   W    ┼   E    ┼   R    ┼   T    ┤                         ├   Y    ┼   U    ┼   I    ┼   O    ┼   P    ┼    -   ┤
+ *  ├   TAB  ┼   Q    ┼   W    ┼   E    ┼   R    ┼   T    ┤                         ├   Y    ┼   U    ┼   I    ┼   O    ┼   P    ┼  DEL   ┤
  *  ├────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┤
  *  ├   ESC  ┼   A    ┼   S    ┼   D    ┼   F    ┼   G    ┤ ╭─────────╮ ╭─────────╮ ├   H    ┼   J    ┼   K    ┼   L    ┼   ;    ┼    '   ┤
  *  ├────────┼────────┼────────┼────────┼────────┼────────┤ ├         ┤ ├         ┤ ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -47,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  */
 [_QWERTY] = LAYOUT(
-  KC_GRV,         KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+  KC_GRV,         KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TILD,
+  KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   LCTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  LSFT_T(KC_CAPS),KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_CAD,  _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT,
+  LSFT_T(KC_CAPS),KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
                           KC_LALT, KC_LGUI,  MO(_LOWER),   KC_SPC,  KC_BSPC, MO(_RAISE), KC_MENU, KC_RCTL
 ),
 
@@ -97,11 +94,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *
  */
  [_RAISE] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______,  _______,  KC_PLUS, KC_MINS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_BSLS,
-                             _______, _______, _______,  _______,  _______,  _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, _______,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   LCTL(LALT(KC_DEL)),   _______,  KC_PLUS, KC_MINS, KC_PLUS,  KC_LBRC, KC_RBRC, KC_BSLS,
+                             _______, _______, _______,  _______,             _______,  _______, _______, _______
 ),
 
 /*
